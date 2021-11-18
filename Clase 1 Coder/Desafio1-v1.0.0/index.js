f2f5c09d33fc2bc3dace1a2a1dd1101309bd59ab
Telayna-I <telayna98@gmail.com>
2021-11-18 20:03:35 -0300

    ejercicio 1 v-1.0.0

---
+++ Clase 1 Coder/Desafio1-v1.0.0/index.js
@@ -0,0 +1,18 @@
+//                    PARTE 1
+
+// let nombre = prompt("Ingrese su nombre: ");
+// document.write(`¡Hola, ${nombre}!`);
+
+//                    PARTE 2
+// let anio = parseInt(prompt("Ingrese el año en que nacio: "));
+// let anio_actual = 2021;
+// let edad = anio_actual - anio;
+// document.write(`Su edad acutal es: ${edad} `);
+
+//                    PARTE 3
+let nombre = prompt("Ingrese su nombre: ");
+let apellido = prompt("Ingrese su apellido: ");
+
+let concat = nombre + " " + apellido;
+
+document.write(`¡Hola, ${concat} !`);
