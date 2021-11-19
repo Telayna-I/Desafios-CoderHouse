8b1e3054d911beec7325e7aa0cc88f89d9d423dd
Telayna-i <telayna98@gmail.com>
2021-11-19 16:20:30 -0300

    Desafio2-v1.1.0

---
+++ Clase 2 Coder/Desafio2-v1.1.0/index.js
@@ -0,0 +1,32 @@
+//                 PARTE 1
+
+// let numero = parseInt(prompt("Ingrese un numero: "));
+// if(numero > 1000){
+//     alert("El numero es mayor a 1000");
+// }else{
+//     alert("El numero no cumple con las condiciones.");
+// }
+
+
+
+//                       PARTE 2
+
+// let palabra = prompt("Ingrese una palabra: ");
+// if(palabra == "hola" || palabra == "HOLA"){
+//     alert("Acertaste");
+// }else{
+//     alert("Fallaste");
+// }
+
+
+
+
+//                       PARTE 3
+
+
+let numero = parseInt(prompt("Ingrese un numero: "));
+if (numero >= 10 && numero <= 50){
+    alert("El numero cumple con las condiciones");
+}else{
+    alert("El numero no cumple con las condiciones");
+}
