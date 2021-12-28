907c34be4fdb1c21984d9b09a7ef58b77e197563
Telayna-I <telayna98@gmail.com>
2021-12-28 11:23:13 -0300

    funcionalidad a presupuestos y parte de encargues

--- Proyecto-Final/index.js
+++ Proyecto-Final/index.js
@@ -5,98 +5,103 @@ const elaborados = [
     {producto: "Tarta de coco", precio_por_kilo: 850},
     {producto: "Tarta de fruta", precio_por_kilo: 850},
     {producto: "Lemon pie", precio_por_kilo: 850},
-];
+]
 
 const encargues = [];
 
+
 class Presupuesto{
-    constructor(peso_requerido, id, precio_por_kilo){
+    constructor(nombre,peso_requerido, precio_por_kilo, nombre_producto){
+        this.nombre = nombre;
         this.peso_requerido = peso_requerido; 
-        this.id = id;
+        this.nombre_producto = nombre_producto;
         this.precio_por_kilo = precio_por_kilo;
     }
 
     presupuestar(){
-        return this.peso_requerido * this.precio_por_kilo;
+        let precio_pro = document.getElementById("precio_pro");
+        let titulo_resul = document.getElementById("titulo-resul");
+        titulo_resul.innerHTML = `Estimad@ ${this.nombre}`;
+        precio_pro.innerHTML = `El precio final por ${this.peso_requerido} kilos de ${this.nombre_producto} es de $${this.precio_por_kilo * this.peso_requerido}`;
+        //return this.peso_requerido * this.precio_por_kilo;
     }
 
-    listar(){
-        elaborados.forEach(elaborado => {
-            console.log(`Producto: ${elaborado.producto} \n Precio: $${elaborado.precio_por_kilo} el kilo `)
-        });
-    }
+    
+    // listar(){
+    //     elaborados.forEach(elaborado => {
+    //         console.log(`Producto: ${elaborado.producto} \n Precio: $${elaborado.precio_por_kilo} el kilo `)
+    //     });
+    // }           
 }
 
 class Pedido{
-    constructor(nombre_producto, kilos,  fecha, nombre, senia, telefono, nota, precio_producto){
+    constructor(nombre_producto, kilos,  fecha, nombre, telefono, nota, precio_producto){
         this.nombre_producto = nombre_producto;
         this.kilos = kilos;
         this.fecha = fecha;
         this.nombre = nombre;
-        this.senia = senia;
         this.telefono = telefono;
         this.nota = nota;
         this.precio_producto = precio_producto;
     }
 
     encargar(){
-        encargues.push({nombre: this.nombre, telefono: this.telefono, modelo: this.nombre_producto, kilos: this.kilos, nota: this.nota, senia: this.senia, fecha: this.fecha});
+        encargues.push({nombre: this.nombre, telefono: this.telefono, producto: this.nombre_producto, kilos: this.kilos, nota: this.nota, fecha: this.fecha});
+        for(let index in encargues){
+            encargues[index].indice = index;
+            localStorage.setItem("pedidos", JSON.stringify(encargues))
+        }
     }
 
-    printOrder(){
-        console.log(`
-        Nombre:${encargues[0].nombre}
-        Telefono: ${encargues[0].telefono}
-        Modelo: ${encargues[0].modelo}
-        Kilos: ${encargues[0].kilos}
-        Nota: ${encargues[0].nota}
-        Seña: ${encargues[0].senia}
-        Fecha: ${encargues[0].fecha}
-        Precio aproximado: ${this.kilos * this.precio_producto}`)
-    }
+}
 
+const btn_calcular = document.getElementById("btn_calcular");
+
+btn_calcular.onclick = (e) =>{
+    e.preventDefault();
+    presupuesto();
+    nombre_pr.value = "";
+    kilos_pr.value ="";
+    producto_pr.value ="";
+}
+
+const btn_encargar = document.getElementById("btn_encargar");
+
+btn_encargar.onclick = (e) =>{
+    e.preventDefault();
+    pedidos();
+    nombre_pe.value = "";
+    kilos_pe.value ="";
+    producto_pe.value ="";
 }
 
 function presupuesto(){
-    const pregunta = parseInt(prompt(`Que precio desea calcular?: \n\n 1) Torta de cumpleaños. \n 2) Pasta frola. \n 3) Postres. \n 4) Tarta de coco. \n 5) Tarta de fruta. \n 6) Lemon pie.`));
-    const peso = parseFloat(prompt("Ingrese el peso que desea calcular: "));
-    let precio_kilo = elaborados[pregunta-1];
+    const nombre_pr = document.getElementById("nombre_pr").value;
+    const kilos_pr = parseFloat(document.getElementById("kilos_pr").value);
+    const producto_pr = parseInt(document.getElementById("producto_pr").value);
+    let precio_kilo = elaborados[producto_pr-1];
     precio_kilo = precio_kilo.precio_por_kilo
-    let nombre_producto = elaborados[pregunta-1];
+    let nombre_producto = elaborados[producto_pr-1];
     nombre_producto = nombre_producto.producto;
-    let presup1 = new Presupuesto(peso,pregunta,precio_kilo);
-    console.log(`El precio final por ${peso} kilos de ${nombre_producto} es de $` + presup1.presupuestar());
-    let extra = prompt(`Desea Conocer la lista de precios? \n 1) Si. \n 2)No.`)
-    if (extra == 1){
-        presup1.listar();
-        alert("Gracias por su consulta !")
-    }else{
-        alert("Gracias por su consulta !")
-    }
+    let presup = new Presupuesto(nombre_pr,kilos_pr,precio_kilo, nombre_producto);
+    presup.presupuestar();
 }
 
 function pedidos(){
-    let nombre = prompt("Ingrese nombre y apellido: ");
-    let telefono = prompt("Ingrese su Telefono: ");
-    let objeto = parseInt(prompt(`Que Desea encargar?: \n\n 1) Torta de cumpleaños. \n 2) Pasta frola. \n 3) Postres. \n 4) Tarta de coco. \n 5) Tarta de fruta. \n 6) Lemon pie.`));
-    let nombre_producto = elaborados[objeto-1];
+    let nombre_pe = document.getElementById("nombre_pe").value;
+    let telefono_pe = document.getElementById("telefono_pe").value;
+    let producto_pe = document.getElementById("producto_pe").value;
+    let nombre_producto = elaborados[producto_pe-1];
     nombre_producto = nombre_producto.producto;
-    let precio_producto = elaborados[objeto-1];
+    let precio_producto = elaborados[producto_pe-1];
     precio_producto = precio_producto.precio_por_kilo;
-    let kilos = parseFloat(prompt("Ingrese la cantidad de kilos: "));
-    let nota = prompt("Ingrese detalles del pedido: ");
-    let senia = parseFloat(prompt("Cuanto deja de seña?: "));
-    let fecha = prompt("Para que dia lo quiere?: ");
+    let kilos_pe = parseFloat(document.getElementById("kilos_pe")).value;
+    let nota_pe = document.getElementById("nota_pe").value;
+    let fecha_pe = document.getElementById("fecha_pe").value;
     
-    let pedido1 = new Pedido(nombre_producto, kilos,  fecha, nombre, senia, telefono, nota, precio_producto);
+    let pedido1 = new Pedido(nombre_producto, kilos_pe,  fecha_pe, nombre_pe, telefono_pe, nota_pe, precio_producto);
     pedido1.encargar();
-    pedido1.printOrder();
+   
+    //console.log(encargues);
+    //pedido1.printOrder();
 }
-
-// const desicion = parseInt(prompt("Que operacion desea realizar?:\n 1) Pesupuesto. \n 2) Encargue."))
-// if(desicion == 1){
-//     presupuesto();
-// }else{
-//     pedidos();
-
-// }
